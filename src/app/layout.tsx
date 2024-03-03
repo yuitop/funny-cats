dbf7089aecd89ca5664352122ea43e1080ba404e
yuitop <near.zill@mail.ru>
2024-03-03 12:26:14 +0300

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0" />
+
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,173 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react"
+import { PictureLoader } from "./PictureLoader"
+import { json } from "stream/consumers"
+
+const ImageSlot = ({ src, style }: any) => {
+  return (
+    <div className={"flex " + style}>
+      <img className="rounded-md flex-1 justify-items-stretch object-cover" src={src}></img>
+    </div>
+  )
+}
+
+const OutlinedPhoto = ({ source, label }: any) => {
+  //className="border-white border-8 rounded-md bg-white">
+
+  return <div>
+    <img className="object-fill max-h-64 border-0 rounded-md" src={source} />
+    <div className="pt-4 font-mono text-center">{label}</div>
+  </div>
+}
+
+const RandomCat = () => {
+  const [picture, setPicture] = useState("")
+  const [count, setCount] = useState(0)
+
+  const randomCat = () => {
+    fetch("https://cataas.com/cat?json=true")
+      .then(data => {
+        return data.json();
+      })
+      .then(json => {
+        let id = json._id;
+        setPicture("https://cataas.com/cat/" + id)
+      }).catch(e => { })
+  }
+
+  useEffect(randomCat, []);
+  useEffect( () => {
+    fetch("https://cataas.com/api/count")
+    .then( data => data.json() )
+    .then( json => {
+      setCount(json.count)
+    } );
+  }, [] )
+
+  return <div className="flex flex-col pt-16 pb-16 pr-8 pl-8 h-fit min-h-screen gap-8">
+    <div className="text-black font-mono text-4xl text-center font-bold">CAT AS A SERVICE</div>
+
+    <div className="text-black font-mono text-body md:text-2xl md:text-center text-justify">Для работы сайта используется API cataas.com. Этот сайт предоставляет более {count-1} фото со смешными котами. Каждый кот уникален и совсем не похож на другого.</div>
+
+    <div></div>
+
+    <div className="flex flex-1 flex-col justify-center items-center gap-8">
+      <img className="rounded-md h-64" src={picture}/>
+      <button onClick={randomCat} className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md w-fit"> Дайте мне случайного кота </button>
+    </div>
+  </div>
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+  return <main>
+
+    <div className="flex h-fit min-h-screen">
+
+      {/* Лево */}
+      <div className="flex-1 flex flex-col md:mr-16">
+        <div className="ml-4 mt-4 select-none flex gap-2">
+          <span className="material-symbols-outlined" style={{ fontSize: 48 }}> mood </span>
+          <span className="font-mono font-bold self-center text-2xl">Ыыы коты</span>
+        </div>
+
+        <div className="flex-1 flex flex-col items-center md:items-start justify-center gap-4">
+          <div className="text-center md:text-left text-white font-mono text-4xl md:text-8xl w-fit pr-8 pl-4 rounded-md ml-4
+          bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Фото со<br />смешными<br />котами
+          </div>
+          <span className="font-mono ml-8 text-2xl">Так смешно</span>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Право */}
+      <div className="flex-1 object-cover hidden md:grid grid-cols-3 grid-rows-3 gap-x-2 gap-y-2 p-2 h-screen">
+
+
+        <ImageSlot src="https://cataas.com/cat/fgsrtdE4IVTV6dp6" />
+        <ImageSlot style="row-span-2" src="https://cataas.com/cat/1TYt4A7YqwaeMUEF" />
+        <ImageSlot style="-mb-8" src="https://cataas.com/cat/1si02A2ZNdeNH3yo" />
+
+        <ImageSlot style="-ml-16" src="https://cataas.com/cat/xk5oZn0fNpfD5J1Q" />
+        <ImageSlot style="mt-8 -mb-8" src="https://cataas.com/cat/OuA1OJrpLqk9156q" />
+
+        <ImageSlot style="col-span-2 ml-8" src="https://cataas.com/cat/NKeOpRP8H27fyhSO" />
+        <ImageSlot style="mt-8" src="https://cataas.com/cat/xDhz2ykbH8wXJw9X" />
+
+      </div>
+      {/* <img className="flex-1 object-cover hidden md:block" src="https://cataas.com/cat/pQ9yAWvYnet83GRN"></img> */}
+
+    </div>
+
+    <div className="flex flex-col items-stretch pt-16 pb-16 pl-4 pr-4
+     bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+
+      <span className="text-white font-mono text-4xl text-center font-bold">Что мы?</span>
+      <span className="text-white font-mono text-body md:text-2xl mt-8 text-justify md:text-center">Добро пожаловать на [вставьте имя сайта] — ваш ежедневный источник смеха и радости, приправленный шалостями наших мурлыкающих друзей. Мы здесь, чтобы делиться смехом, любовью и безграничной милотой через смешные фотографии котов со всего мира.</span>
+
+    </div>
+
+    {/* <div className="flex-1 grow"></div>
+
+      <div className="relative top-24 flex-1 flex gap-8 justify-evenly">
+
+        <OutlinedPhoto label="Кот спит" source="https://cataas.com/cat/mACGUDVn2tlR1z2B"/>
+        <OutlinedPhoto label="Кот просыпается" source="https://cataas.com/cat/FBENjGWtqyGWqRYA"/>
+        <OutlinedPhoto label="Кот работает" source="https://cataas.com/cat/FhBXgwOJCesSPRtg"/>
+
+      </div> */}
+
+    <div className="flex flex-col p-16 h-fit min-h-screen gap-16">
+      <div className="text-black font-mono text-4xl text-center font-bold">Как это работает?</div>
+      <div className="flex-1 flex flex-col md:flex-row items-center justify-around gap-16">
+
+        <OutlinedPhoto label="Кот спит" source="https://cataas.com/cat/mACGUDVn2tlR1z2B" />
+
+        <div className="hidden md:block"><span style={{ fontSize: 48 }} className="material-symbols-outlined mb-8"> chevron_right </span></div>
+
+        <OutlinedPhoto label="Кот просыпается" source="https://cataas.com/cat/FBENjGWtqyGWqRYA" />
+
+        <div className="hidden md:block"><span style={{ fontSize: 48 }} className="material-symbols-outlined mb-8"> chevron_right </span></div>
+
+        <OutlinedPhoto label="Кот работает" source="https://cataas.com/cat/FhBXgwOJCesSPRtg" />
+
+      </div>
+    </div>
+
+    <div className="p-16 pt-16 pb-16 pl-4 pr-4
+     bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+
+      <div className="text-white font-mono text-4xl text-center font-bold">Не надо связываться с нами!</div>
+
+      <div className="flex flex-col md:flex-row items-center md:justify-around mt-16 gap-8">
+
+        <div className="flex flex-row items-center text-white gap-4">
+          <span className="material-symbols-outlined"> call </span>
+          <span className="text-body md:text-2xl font-mono"> +7 (967) 730 25-32 </span>
+        </div>
+
+        <div className="flex flex-row items-center text-white gap-4">
+          <span className="material-symbols-outlined"> mail </span>
+          <span className="text-body md:text-2xl font-mono"> mail.mail@mail.ru </span>
+        </div>
+
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    <RandomCat />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+
+    <div className="p-16 pt-16 pb-16 pl-4 pr-4 mb-16
+     bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+
+      <div className="text-white font-mono text-4xl text-center font-bold">Готовы увидеть котов?</div>
+      <div className="text-white font-mono text-body md:text-2xl text-center mt-8">Внизу вы найдете фотографии котов.</div>
+
+    </div>
+
+    <PictureLoader />
+  </main>
 }
